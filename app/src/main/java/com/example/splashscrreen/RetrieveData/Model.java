8051eaf944af559f4afce718a6f52f1b9e9bfc62
ginadurotul <ginadurotul@gmail.com>
2021-06-27 21:15:28 +0700

    First

---
+++ app/src/main/java/com/example/splashscrreen/RetrieveData/Model.java
@@ -0,0 +1,36 @@
+package com.example.splashscrreen.RetrieveData;
+
+public class Model {
+    String id, name, address;
+    public Model(){}
+
+    public Model(String id,String name, String address){
+        this.id =id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
